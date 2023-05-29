651ecda582bcc40df95e2326c9a82c163e1efc18
Chris Hatch <hatch@tuta.io>
2023-05-29 22:09:18 +0700

    experimental edge function that dumps headers - used this to check if the supabase webhook was sending a signature header (AWS ApiGateway logs trunctation meant i couldn't see it with the microservce)

---
+++ experiments/supabase-edge-fns/supabase/invoke-remote.sh
@@ -0,0 +1,4 @@
+curl -L -X POST 'https://rkzgrxssutcwrauaazjz.functions.supabase.co//supabase-db-hook-edge-fn' \
+    -H 'Authorization: Bearer <JWT here>' \
+    -H 'x-custom-hatch: xyz' \
+    --data '{"name":"linux"}'
