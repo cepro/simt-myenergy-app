b291fc6d018463151f4a1d2f11a5407675874255
Chris Hatch <hatch@tuta.io>
2023-07-05 04:57:20 +0700

    update the static page build: - remove subpath hacks as we are now on a domain without page - add favicon.ico to override flutterflow one

--- github-pages/build.sh
+++ github-pages/build.sh
@@ -9,28 +9,19 @@ mkdir $EXPORT_HOME
 flutterflow export-code --project myenergy-billing-prototype-wbk8s3 --include-assets --dest $EXPORT_HOME 2>/dev/null
 cd $APP_HOME
 
-echo "\nSetting the <base> in web/index.html ..."
-sed -i -e 's/<base[^>]*>/<base href="\/simt-myenergy-gh-pages\/">/' web/index.html
-
 echo "\nBuilding flutter app ..."
 flutter build web
 
 HTML=build/web/index.html
 echo "\nAdding SPA github pages snippet to $HTML ..."
 
-SPLIT_LINE_NUM=`grep -n "</head>" $HTML | cut -d: -f1`
-NUM_LINES=`wc -l $HTML | cut -d" " -f1`
-
-NEW_HTML=$HTML.new
-head -`expr $SPLIT_LINE_NUM - 1` $HTML > $NEW_HTML
-cat $THIS_PROJECT/gh-pages-hack-snippet.html >> $NEW_HTML
-tail -`expr $NUM_LINES - $SPLIT_LINE_NUM + 1` $HTML >> $NEW_HTML
-mv $NEW_HTML $HTML
-
 echo "\nCopying build/web to $GH_PAGES_HOME ..."
 rm -rf $GH_PAGES_HOME/*
 cp -r build/web/* $GH_PAGES_HOME
 cd $GH_PAGES_HOME
+
+echo "\nAdd our favicon.ico"
+cp $THIS_PROJECT/favicon.ico $GH_PAGES_HOME
 
 git restore README.md manifest.json
 git status
