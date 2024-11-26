872c1bb808a29007e2fbf1928f634b8acd7a810f
Chris Hatch <hatch@tuta.io>
2024-11-26 11:46:21 +0700

    implement new versioning system: - maintain version and build in new myenergy.version - inject combination into pubspec.yaml after build - inject build number into index.html on end of flutter_bootstrap.js URI

--- bin/library.sh
+++ bin/library.sh
@@ -3,15 +3,31 @@
 BIN_DIR=`dirname $0`
 PROJECT_ROOT=`dirname $BIN_DIR`
 
+SYS_INFO_PAGE_SOURCE_PATH=lib/pages/sys_info_page/sys_info_page_widget.dart
+VERSION_FILE=$PROJECT_ROOT/myenergy.version
+
 getGitCommit() {
   echo `git rev-parse --short HEAD`
+}
+
+getAppVersion() {
+  grep version= $VERSION_FILE | sed -e 's/version=//'
+}
+
+getBuildNumber() {
+  grep build= $VERSION_FILE | sed -e 's/build=//'
+}
+
+incrementBuildNumber() {
+  BUILD_NUMBER=`getBuildNumber`
+  NEW_BUILD_NUMBER=`expr $BUILD_NUMBER + 1`
+  sed -i -e "s/build=.*/build=$NEW_BUILD_NUMBER/" $VERSION_FILE
+  echo $NEW_BUILD_NUMBER
 }
 
 getFlutterVersion() {
   echo `flutter --version | grep "^Flutter" | sed -e 's/^Flutter \([^ ]*\).*/\1/'`
 }
-
-SYS_INFO_PAGE_SOURCE_PATH=lib/pages/sys_info_page/sys_info_page_widget.dart
 
 setSysInfoEnvironment() {
   ENV=$1
