a75764b83bb70f3a27b3d6d32c417d8a7794008c
Chris Hatch <devcebfaff@proton.me>
2025-10-24 06:36:50 +1000

    stop using myenergy.version and instead us pubspec.yaml

--- bin/library.sh
+++ bin/library.sh
@@ -3,24 +3,25 @@
 BIN_DIR=`dirname $0`
 PROJECT_ROOT=`dirname $BIN_DIR`
 
-VERSION_FILE=$PROJECT_ROOT/myenergy.version
+PUBSPEC_FILE=$PROJECT_ROOT/app/pubspec.yaml
 
 getGitCommit() {
   echo `git rev-parse --short HEAD`
 }
 
 getAppVersion() {
-  grep version= $VERSION_FILE | sed -e 's/version=//'
+  grep "^version:" $PUBSPEC_FILE | sed -e 's/version: *//' -e 's/+.*//'
 }
 
 getBuildNumber() {
-  grep build= $VERSION_FILE | sed -e 's/build=//'
+  grep "^version:" $PUBSPEC_FILE | sed -e 's/.*+//'
 }
 
 incrementBuildNumber() {
+  VERSION=`getAppVersion`
   BUILD_NUMBER=`getBuildNumber`
   NEW_BUILD_NUMBER=`expr $BUILD_NUMBER + 1`
-  sed -i -e "s/build=.*/build=$NEW_BUILD_NUMBER/" $VERSION_FILE
+  sed -i -e "s/^version:.*/version: $VERSION+$NEW_BUILD_NUMBER/" $PUBSPEC_FILE
   echo $NEW_BUILD_NUMBER
 }
 
